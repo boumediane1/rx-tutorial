2fe2fa72768be623f20989de3755ccbb067e88c5
Othmane Boumediane <boumediane.othmane@gmail.com>
2023-08-30 10:36:18 +0100

    sync stream

--- hello.test.ts
+++ hello.test.ts
@@ -1,25 +1,23 @@
-import { combineLatest, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 
-it('works', () => {
-  const a = new Subject<number>();
-  const b = new Subject<number>();
+const s = new Subject<number>();
+s.subscribe((value) => console.log(value, process.hrtime()));
 
-  const c = combineLatest([a, b]);
-
-  // consumer
-  c.subscribe(([a, b]) => console.log(a + b));
-
-  // producer
-  a.next(3);
-  b.next(3);
-  b.next(4);
-});
+console.log('nexting 1...');
+s.next(1);
+console.log('nexting 2...');
+s.next(2);
+console.log('nexting 3...');
+s.next(3);
 
 // Expected output:
-// hello world
-// goodbye world
+// nexting 1...
+// 1 [ 38423, 597598882 ]
+// nexting 2...
+// 2 [ 38423, 599097102 ]
+// nexting 3...
+// 3 [ 38423, 599250068 ]
 
-// when the value 4 is emitted into stream b, it triggers an update in c
-// because c combines the latest values from both streams a and b.
-// The subscriber receives the previous value of a (which is 3, the latest on a)
-// and the latest value on b (which is 4) and then computes and logs the result.
+// When calling 'next', the subscription callback is immediately executed,
+// logging the emitted value along with the high-resolution time from
+// the Node.js 'process.hrtime()' function.
