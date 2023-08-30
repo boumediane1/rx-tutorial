d24c56c1c80f81ada34a94fff5d301f59c9b79f2
Othmane Boumediane <boumediane.othmane@gmail.com>
2023-08-30 15:58:34 +0100

    subjects behavior

--- hello.test.ts
+++ hello.test.ts
@@ -1,4 +1,19 @@
-import { interval } from 'rxjs';
+import { Subject } from 'rxjs';
 
-// the program exits right away if there's no subscription
-const int = interval(1000);
+// whether we subscribe or not, the code is still running
+// and the program will not exit right away
+// The code does the same thing regardless of the number of subscribers
+const s = new Subject<number>();
+
+console.log('nexting 1...');
+s.next(1);
+
+setTimeout(() => {
+  console.log('nexting 1...');
+  s.next(2);
+}, 1000);
+
+setTimeout(() => {
+  console.log('nexting 2...');
+  s.next(3);
+}, 2000);
