053b366cbca5dc45223f5159aad36908a8290ff6
Othmane Boumediane <boumediane.othmane@gmail.com>
2023-08-31 13:21:08 +0100

    add delay operator

--- hello.test.ts
+++ hello.test.ts
@@ -1,8 +1,9 @@
-import { filter, interval, map } from 'rxjs';
+import { delay, filter, interval, map } from 'rxjs';
 
 interval(500)
   .pipe(
     filter((value) => value % 2 === 0),
     map((value) => value * 2),
+    delay(5000),
   )
   .subscribe(console.log);
