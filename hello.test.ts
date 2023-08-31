a53e316766aa799a96d777dd0cc35a652b09bc89
Othmane Boumediane <boumediane.othmane@gmail.com>
2023-08-31 22:21:24 +0100

    mergeAll behavior

--- hello.test.ts
+++ hello.test.ts
@@ -2,7 +2,7 @@ import { interval, map, mergeAll, take } from 'rxjs';
 
 const numbers = interval(1000).pipe(take(3));
 
-const letters = interval(250).pipe(
+const letters = interval(500).pipe(
   take(3),
   map((i) => String.fromCharCode(i + 97)),
 );
@@ -14,10 +14,10 @@ numbers.pipe(map(project), mergeAll()).subscribe(console.log);
 // Expected output:
 // [ 0, 'a' ]
 // [ 0, 'b' ]
-// [ 0, 'c' ]
 // [ 1, 'a' ]
+// [ 0, 'c' ]
 // [ 1, 'b' ]
-// [ 1, 'c' ]
 // [ 2, 'a' ]
+// [ 1, 'c' ]
 // [ 2, 'b' ]
 // [ 2, 'c' ]
