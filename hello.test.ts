8fcd7153b538ce53636e9b6af036ee263b0a4d24
Othmane Boumediane <boumediane.othmane@gmail.com>
2023-08-31 14:12:31 +0100

    implement custom operator

--- hello.test.ts
+++ hello.test.ts
@@ -1,9 +1,10 @@
 import { delay, filter, interval, map } from 'rxjs';
 
-interval(500)
-  .pipe(
+const customOperator = () => () =>
+  interval(500).pipe(
     filter((value) => value % 2 === 0),
     map((value) => value * 2),
-    delay(5000),
-  )
-  .subscribe(console.log);
+    delay(3000),
+  );
+
+interval(500).pipe(customOperator()).subscribe(console.log);
